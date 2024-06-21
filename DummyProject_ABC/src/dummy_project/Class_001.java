86ee2d831356b01551f5536bfc8b6332834fc414
Ashvini94 <ashvinipofare01@gmail.com>
2024-06-21 17:26:53 +0530

    dummy project is added

---
+++ DummyProject_ABC/src/dummy_project/Class_001.java
@@ -0,0 +1,11 @@
+package dummy_project;
+
+public class Class_001 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello,I am here");
+
+	}
+
+}
---
+++ DummyProject_ABC/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vaibh
+ *
+ */
+module DummyProject_ABC {
+}
